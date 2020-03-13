699b4053f3e2c09191973455647f447ab0de201a
Han Byeong Ok <hbok1119@naver.com>
2020-03-13 21:48:26 +0900

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main(void){
+  char arr[3][3][2][30] = {
+    {
+      {"이름", "Ikari Shinji"},
+      {"나이", "14세"},
+      {"키", "156cm"}
+    },
+    {
+      {"이름", "Soryu Asuka Langley"},
+      {"나이", "14세"},
+      {"키", "157cm"}
+    },
+    {
+      {"이름", "Ayanami Rei"},
+      {"나이", "14세"},
+      {"키", "149cm"}
+    }
+  };
+
+  for(
+  }
+}
